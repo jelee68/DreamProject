2bd98460208bc05e05f7ecd47ee6869da79504e4
“ppoyang <yourmyeden@gmail.com>
2018-08-14 15:09:30 +0900

    rent

--- DreamLibrary/src/main/java/com/triple/dreamlib/RentController.java
+++ DreamLibrary/src/main/java/com/triple/dreamlib/RentController.java
@@ -1,5 +1,23 @@
 package com.triple.dreamlib;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RentController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
+	
+	@RequestMapping("/rent_manager")
+	public String rent_main() {
+		return "rent_manager";
+	}
 
 }
